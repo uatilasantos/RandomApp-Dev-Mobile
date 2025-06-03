133acbc29344b655d14457e4bfedf2923129838e
Luiz <luizotaviossilva@hotmail.com>
2025-06-03 20:33:16 -0300

    Fix: Botões funcionando

---
+++ app/src/main/java/com/impacta/myapplication/FiltroActivity.kt
@@ -0,0 +1,25 @@
+package com.impacta.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FiltroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filtro)
+
+        val botaoVoltar: ImageButton = findViewById(R.id.botaoVoltar)
+
+        botaoVoltar.setOnClickListener{
+            finish()
+
+        }
+
+    }
+}
