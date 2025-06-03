8557f50f00d4a4e48927ed1a6d810e5ab2dcf56c
Luiz <luizotaviossilva@hotmail.com>
2025-06-03 11:34:31 -0300

    Feat: Subindo os arquivos do pré projeto

---
+++ app/src/main/java/com/impacta/myapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.impacta.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.impacta.myapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val startButton: Button = findViewById(R.id.startButton)
+        startButton.setOnClickListener {
+
+        }
+    }
+}
